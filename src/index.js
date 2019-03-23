8c02bade77795a898ab2583ffd6901639f615fb0
Ivan Tsai <oscar60310@gmail.com>
2019-03-23 15:53:02 +0800

    add babel-loader, configure babel presets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('test')
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const app = document.getElementById('app');
+render(<h1> Hello, I'm here</h1>,app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   context: path.resolve('src/'),
   entry: {
     client: './index.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: './[name].bundle.js',
-  }
+    filename: './[name].[hash:8].js',
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    })
+  ]
 }
