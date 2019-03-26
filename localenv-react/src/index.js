810c3408b18d8e9259ae5f9eaa56c4e5271b4e3c
Kai <b10223052@yuntech.org.tw>
2019-03-26 14:16:22 +0800

    add react code and set babel in webpack.config

--- localenv-react/src/index.js
+++ localenv-react/src/index.js
@@ -1,3 +1,11 @@
-(function () {
-  console.log("Hello webpack");
-}());
+import React from "react";
+import ReactDOM from "react-dom";
+
+let HelloWorld = function() {
+  return <h1> Hello, I am here </h1>
+}
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.getElementById("root")
+);
--- localenv-react/webpack.config.js
+++ localenv-react/webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: path.join(__dirname,'src','index.js'),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname,'build'),
+    path: path.join(__dirname, 'build'),
     filename: 'index.bundle.js'
   },
   mode: process.env.NODE_ENV || 'development',
   resolve: {
     modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }]
+  },
   devServer: {
-    contentBase: path.join(__dirname,'src')
+    contentBase: path.join(__dirname, 'src')
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname,'src','index.html')
+      template: path.join(__dirname, 'src', 'index.html')
     })
   ]
 };
