b2255febc59d254a2b7080c21935e6fd849183e2
PenguinRun <shanban820@gmail.com>
2019-03-26 12:18:42 +0800

    add index.js and containers folder

---
+++ src/containers/Hello/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Hello.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Hello">
+        <h1> Hello, I'm there </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./containers/Hello/Hello";
+ReactDOM.render(<Hello />, document.getElementById("root"));
