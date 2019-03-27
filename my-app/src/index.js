ee52375e28df8deef5b9da40761983a008d2ec11
chris <dwatow@gmail.com>
2019-03-27 10:15:55 +0800

    delete in src/ files, modify index.js as hello message

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <HelloMessage name="Chris" />,
+  document.getElementById('root')
+);
