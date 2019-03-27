aba5fc5e7175faca350a7388c407004921c85520
chris <dwatow@gmail.com>
2019-03-27 15:57:11 +0800

    setup mode for webpack

---
+++ react-init/build/webpack.common.js
@@ -0,0 +1,8 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  plugins: [new HtmlWebpackPlugin({
+    template: 'public/index.html',
+  })]
+};
