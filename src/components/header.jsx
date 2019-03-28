426a3d958a3711df748c3b9740e8861fee9bf4e4
Ivan Tsai <oscar60310@gmail.com>
2019-03-28 20:43:26 +0800

    finish header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
+import bagImage from '../images/bag_img.png';
+
 const HeaderComponent = () => {
-  return <div className="header">
-    <div>PRETTY GAL</div>
+  return <div className="header px-52">
+    <div className="icon font-size-20">PRETTY GAL</div>
+    <div>Home</div>
+    <div>Shop</div>
+    <div>Sale</div>
+    <div>Customer Care</div>
+    <div>Stockists</div>
+    <img src={bagImage} alt="blue bag" />
   </div>
 }
 export default HeaderComponent;
