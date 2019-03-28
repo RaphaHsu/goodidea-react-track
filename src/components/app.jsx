a71481775848fe23ccfef44f3c724f6cd01140b9
Ivan Tsai <oscar60310@gmail.com>
2019-03-28 21:11:33 +0800

    slogan finish

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader/root'
 import Header from './header';
 import Media from './media';
+import Slogan from './slogan';
 
 const AppComponent = () => {
   return <div>
     <Header />
     <Media />
+    <Slogan />
   </div>
 }
 export default hot(AppComponent);
--- src/components/slogan.jsx
+++ src/components/slogan.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 const SloganComponent = () => {
-  return <div>
+  return <div className="slogan center-child p-52">
+    <div className="l1 font-size-38">YEAR ROUND</div>
+    <div className="dash my-20"></div>
+    <div className="font-size-24">Must Have Items</div>
   </div>
 }
 export default SloganComponent;
