f378e22b41be8419939f9efaf1bee1d34dd3f589
Ivan Tsai <oscar60310@gmail.com>
2019-03-28 21:05:38 +0800

    media component finish

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root'
 import Header from './header';
+import Media from './media';
+
 const AppComponent = () => {
   return <div>
     <Header />
+    <Media />
   </div>
 }
 export default hot(AppComponent);
--- src/components/media.jsx
+++ src/components/media.jsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 const MediaComponent = () => {
-  return <div>
+  return <div className="media">
+    <div style={{ flex: '1 1 0' }} className="center-child">
+      <div className="title-container center-child p-24">
+        <div className="title font-size-50">FALL & WINTER</div>
+        <button className="px-60 py-8 font-size-16 mt-24">Shop Now</button>
+      </div>
+    </div>
+    <div className="description center-child">
+      FREE SHIPPING WORLDWIDE
+    </div>
   </div>
 }
 export default MediaComponent;
