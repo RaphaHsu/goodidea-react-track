e3873070d808abe266634d1ab07356e99b363a06
UnaLai <qqq323618@gmail.com>
2019-03-29 20:23:58 +0800

    feat:install babel.webpack.react and create files

---
+++ Q3_T2/src/container/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+// import "./App.css";
+
+class Header extends Component{
+ render(){
+   return(
+     <header>
+       <div class="logo"></div>
+     </header>
+   );
+ }
+}
+
+export default App;
