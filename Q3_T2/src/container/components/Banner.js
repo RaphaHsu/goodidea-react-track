1bf5b794af9853a176754cb8c4ff00f05564634a
UnaLai <qqq323618@gmail.com>
2019-03-29 23:43:01 +0800

    add banner

--- Q3_T2/src/container/components/Banner.js
+++ Q3_T2/src/container/components/Banner.js
@@ -3,8 +3,15 @@ import "../../styles/banner.scss";
 
 class Banner extends Component {
   render() {
-    return (<header>
-    </header>);
+    return (<section>
+      <div class="banner">
+        <div class="banner_intro">
+          <div class="banner_intro_title">FALL & WINTER</div>
+          <button>Shop Now</button>
+        </div>
+      </div>
+      <div class="banner_free">FREE  SHIPPING  WORDWIDE</div>
+    </section>);
   }
 }
 
