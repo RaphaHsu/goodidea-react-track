598d2022c2d0d499642699cbdaf636d2b3e810b5
UnaLai <qqq323618@gmail.com>
2019-03-30 12:47:33 +0800

    finish footer and adjust other css

--- Q3_T2/src/container/components/Footer.js
+++ Q3_T2/src/container/components/Footer.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import "../../styles/footer.scss";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 class Footer extends Component {
   render() {
@@ -7,6 +8,12 @@ class Footer extends Component {
       <div class="container footerContainer">
         <div class="connected">
           <h5>STAY CONNECTED</h5>
+          <div class="connected_icons">
+            <a href="#"><FontAwesomeIcon icon={['fab', 'facebook-f']} color="white" size="2x"/></a>
+            <a href="#"><FontAwesomeIcon icon={['fab', 'twitter']} color="white" size="2x"/></a>
+            <a href="#"><FontAwesomeIcon icon={['fab', 'pinterest-p']} color="white" size="2x"/></a>
+            <a href="#"><FontAwesomeIcon icon={['fab', 'instagram']} color="white" size="2x"/></a>
+          </div>
         </div>
         <div class="email">
           <h5>BE OUR FRIEND</h5>
@@ -18,7 +25,9 @@ class Footer extends Component {
           <p>123-456-7890</p>
           <p>info@mysite.com</p>
         </div>
-        <p class="copyright">© 2023 by PRETTY GAL. Proudly created with <a href="#">Wix.com</a></p>
+        <p class="copyright">© 2023 by PRETTY GAL. Proudly created with 
+          <a href="#">Wix.com</a>
+        </p>
       </div>
     </footer>);
   }
--- Q3_T2/src/index.js
+++ Q3_T2/src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 import Homepage from "./container/Homepage"
 import "./styles/reset.scss";
 import "./styles/_global.scss"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faIgloo ,faTrain,faDrawPolygon} from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF ,faInstagram,faTwitter,faPinterestP} from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(faFacebookF,faInstagram,faTwitter,faPinterestP)
 
 ReactDOM.render(<Homepage />, document.getElementById("root"));
