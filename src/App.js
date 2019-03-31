1e591cf07a34002e5a7117994bbff9138114fac9
kira <kira@kiradeMacBook-Pro.local>
2019-03-31 19:53:51 +0800

    finish banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Banner />
       </div>
     )
   }
