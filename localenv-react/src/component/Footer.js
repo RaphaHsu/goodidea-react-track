4ad5b13bbb74e7ff85635d87b083bf50198de390
Kai <b10223052@yuntech.org.tw>
2019-03-31 11:58:58 +0800

    set footer template and style/fix banner img width

--- localenv-react/src/component/Footer.js
+++ localenv-react/src/component/Footer.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import igIcon from '../image/ig.png';
+import fbIcon from '../image/fb.png';
+import twitterIcon from '../image/twitter.png';
+import pinterestIcon from '../image/pinterest.png';
+
 
 export default function(){
   return (
-    <div>
-      shit
+    <div className="footer">
+      <div className="footerContent">
+        <div className="footerLayout">
+          <div className="footerConnected">
+            <p className="title">
+              STAY CONNECTED
+            </p>
+            <img src={igIcon}/>
+            <img src={fbIcon}/>
+            <img src={twitterIcon}/>
+            <img src={pinterestIcon}/>
+
+          </div>
+          <div className="footerEmail">
+            <p className="title">
+              BE OUR FRIEND
+            </p>
+            <div className="email">
+              Email
+            </div>
+            <button>
+              Update Me
+            </button>
+            <div className="clearFix">
+            </div>
+          </div>
+          <div className="footerContactUs">
+            <p className="title">
+              NEED ASSISTANCE?
+            </p>
+            <p>
+              123-456-7890
+            </p>
+            <p>
+              info@mysite.com
+            </p>
+          </div>
+        </div>
+        <div className="noticeText">
+          © 2023 by PRETTY GAL. Proludly created with <span>Wix.com</span>
+        </div>
+      </div>
     </div>
   )
 }
