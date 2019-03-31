8ccf1d56468e435e83ed4ab0bec5cfa92472c8d1
henry97113 <henry97113@gmail.com>
2019-03-31 17:22:29 +0800

    import images & define path for styles

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ const htmlPlugin = new HtmlWebpackPlugin({
 });
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
