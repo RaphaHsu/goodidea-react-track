7a1e4254be4055ec5060f2ce3982c451c0ab0af8
Askie Lin <isme408@gmail.com>
2019-04-01 14:59:19 +0800

    新增webpack-dev-server，更動npm start指令，讓它可以Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 const Index = () => {
-  return <div>Hello World!</div>
+  return <div>Hello I'm here...</div>
 }
 
 ReactDOM.render(<Index />, document.getElementById("app"))
