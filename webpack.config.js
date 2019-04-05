732a5323cba32e99641049a327549570732e502b
Ivan Tsai <oscar60310@gmail.com>
2019-04-05 21:10:02 +0800

    rename js files to ts, change webpack config to complie ts files

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
 /* eslint-disable no-undef */
-const path = require('path');
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  context: path.resolve('src/'),
+  context: path.resolve("src/"),
   entry: {
-    client: './index.jsx'
+    client: "./index"
   },
   resolve: {
-    extensions: ['.js','.jsx']
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(j|t)sx?$/,
         use: {
           loader: "babel-loader"
         }
       },
       {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader',
+        loader: "style-loader!css-loader!sass-loader"
       },
       {
         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: 'url-loader',
+        loader: "url-loader",
         query: {
           limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
+          name: "static/media/[name].[hash:8].[ext]"
+        }
+      }
     ]
   },
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: './[name].[hash:8].js',
+    path: path.join(__dirname, "build"),
+    filename: "./[name].[hash:8].js"
   },
   plugins: [
     new HtmlWebPackPlugin({
