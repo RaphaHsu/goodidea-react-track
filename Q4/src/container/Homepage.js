1862d85a5866ca9f44fe887f5d7a2a0d9674b012
UnaLai <qqq323618@gmail.com>
2019-04-06 16:39:48 +0800

    [feat]:add homepage's benner

--- Q4/src/container/Homepage.js
+++ Q4/src/container/Homepage.js
@@ -1,9 +1,14 @@
 import React, {Component} from "react";
+import "../styles/homepage.scss";
+
 import Mainproducts from "./components/Mainproducts.js";
 
 class Homepage extends Component {
   render() {
-    return (<div className="homepage">
+    return (<div className="homepage container">
+      <div className="banner">
+        <h1>好想工作室 X 天瓏書局</h1>
+      </div>
       <Mainproducts></Mainproducts>
     </div>);
   }
