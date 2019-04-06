6aa4292ccc4e253be61a4bb76b2b0f81fe9206c0
UnaLai <qqq323618@gmail.com>
2019-04-07 00:48:29 +0800

    [feat]:adjust icon position

--- Q4/src/container/components/Search.js
+++ Q4/src/container/components/Search.js
@@ -4,11 +4,11 @@ import "../../styles/search.scss";
 
 const Search = (Props) => {
   return (<section className="search container">
-    <form className="search-form">
+    <div className="search-form">
       <label>Book search</label>
-      <input placeholder="請輸入書名" onChange={(e)=>{Props.searchBook(e)}}></input>
       <span><FontAwesomeIcon icon={['fas', 'search']} size="2x"/></span>
-    </form>
+      <input placeholder="請輸入書名" onChange={(e)=>{Props.searchBook(e)}}></input>
+    </div>
   </section>);
 }
 
