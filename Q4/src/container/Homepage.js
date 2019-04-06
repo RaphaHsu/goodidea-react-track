771017323acac0fddf6aa806d4e402bbbcd2e6d8
UnaLai <qqq323618@gmail.com>
2019-04-06 18:52:02 +0800

    [feat]:install axios and get book data

--- Q4/src/container/Homepage.js
+++ Q4/src/container/Homepage.js
@@ -2,15 +2,34 @@ import React, {Component} from "react";
 import "../styles/homepage.scss";
 import Search from "./components/Search.js";
 import MainBooks from "./components/MainBooks.js";
+import axios from 'axios';
 
 class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookData: "",
+      displayBookData:""
+    };
+  }
+  getBookData() {
+    axios.get('https://bookshelf.goodideas-studio.com/api').then(response => {
+      this.setState({
+        bookData:response.data.list,
+        displayData:response.data.list
+      });
+    })
+  }
+  componentDidMount() {
+    this.getBookData();
+  }
   render() {
     return (<div className="homepage container">
       <div className="banner">
         <h1>好想工作室 X 天瓏書局</h1>
       </div>
       <Search></Search>
-      <MainBooks></MainBooks>
+      <MainBooks bookData="this.state.displayData"></MainBooks>
     </div>);
   }
 }
