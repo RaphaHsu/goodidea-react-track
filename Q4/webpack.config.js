c925bc260d85bb17bdf8551070958845c09233b0
kira <kira@kiradeMBP.Home>
2019-04-11 00:06:13 +0800

    install sass loader and url loader

--- Q4/webpack.config.js
+++ Q4/webpack.config.js
@@ -15,7 +15,23 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 500,
+              name: 'images/[name]_[hash:7].[ext]',
+            }
+          }
+        ]
+      },
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
