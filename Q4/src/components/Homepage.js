1c92816103f0f0a4b2d472ad542ffdf3f415c469
kira <kira@kiradeMBP.Home>
2019-04-11 00:32:14 +0800

    fetch API

--- Q4/src/components/Homepage.js
+++ Q4/src/components/Homepage.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import BooksDisplay from "./BooksDisplay"
+import '../styles/Homepage.scss'
+import "../styles/reset.scss";
 
 class Homepage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      booksData: [],
+      filterBooksData: [],
+      isLoaded: false,
+      searchInput: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://bookshelf.goodideas-studio.com/api')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          booksData: data.list,
+          filterBooksData: data.list,
+        })
+      })
+      .catch(error => console.log('Opps', error))
+  }
+
   render() {
+    const { booksData, filterBooksData } = this.state;
     return (
       <div>
-        <h1> Hello, World! </h1>
+        <ul>
+          {booksData.map((book, index) => (
+            <li>{book.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
