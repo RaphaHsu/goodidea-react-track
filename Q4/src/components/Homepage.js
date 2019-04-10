36160c209b2b98a21c3a4e7924f5af0135e22145
kira <kira@kiradeMBP.Home>
2019-04-11 00:00:34 +0800

    building basic react project environment

---
+++ Q4/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
