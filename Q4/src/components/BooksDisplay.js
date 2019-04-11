ae3c9a440e60d9b5bd067ee2b8d2f0cc3c376a7a
kira <kira@kiradeMacBook-Pro.local>
2019-04-11 11:05:21 +0800

    add search function

--- Q4/src/components/BooksDisplay.js
+++ Q4/src/components/BooksDisplay.js
@@ -2,34 +2,52 @@ import React, { Component } from "react"
 import Card from 'react-bootstrap/Card'
 import "../styles/BooksDisplay.scss"
 
-const BooksDisplay = ({ BooksData }) => {
-  return (
-    <section>
-      <div className="data-result">共有{BooksData.length}筆結果</div>
-      <div className="display-container">
-        {BooksData.map((book, index) => (
-          <Card key={book.name} className="book">
-            <div className="book-image-container">
-              <Card.Img variant="top" src={book.image} />
-            </div>
-            <Card.Body className="book-info">
-              <Card.Title className="book-info-bookname">{book.name}</Card.Title>
-              <Card.Text className="book-info-ISBN">
-                ISBN： {book.ISBN}
-              </Card.Text>
-              <Card.Text className="book-info-originprice">
-                原價：NT {book.originPrice}
-              </Card.Text>
-              <Card.Text className="book-info-sellprice">
-                特價：NT {book.sellPrice}
-              </Card.Text>
-              <a className="book-info-more" href={book.link}>詳細資訊</a>
-            </Card.Body>
-          </Card>
-        ))}
+const BooksDisplay = ({ BooksData, keyword }) => {
+
+  if (BooksData.length === 0) {
+    console.log(BooksData)
+    console.log('找不到')
+    return (
+      <div>
+        loading
       </div>
-    </section>
-  )
+    )
+  } else if (BooksData[0] === "查無資料") {
+    console.log(BooksData)
+    return (
+      <div>
+        找不到書籍
+        </div>
+    )
+  } else {
+    return (
+      <section>
+        <div className="data-result">共有{BooksData.length}筆結果</div>
+        <div className="display-container">
+          {BooksData.map((book, index) => (
+            <Card key={book.name} className="book">
+              <div className="book-image-container">
+                <Card.Img variant="top" src={book.image} />
+              </div>
+              <Card.Body className="book-info">
+                <Card.Title className="book-info-bookname">{book.name}</Card.Title>
+                <Card.Text className="book-info-ISBN">
+                  ISBN： {book.ISBN}
+                </Card.Text>
+                <Card.Text className="book-info-originprice">
+                  原價：NT {book.originPrice}
+                </Card.Text>
+                <Card.Text className="book-info-sellprice">
+                  特價：NT {book.sellPrice}
+                </Card.Text>
+                <a className="book-info-more" href={book.link}>詳細資訊</a>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </section>
+    )
+  }
 }
 
 export default BooksDisplay
--- Q4/src/components/Homepage.js
+++ Q4/src/components/Homepage.js
@@ -27,12 +27,37 @@ class Homepage extends Component {
       .catch(error => console.log('Opps', error))
   }
 
+  updateInput() {
+    this.setState({
+      searchInput: event.target.value
+    })
+  }
+
+  filterData() {
+    let filterBooksData = this.state.booksData.filter((book) => {
+      return book.name.match(this.state.searchInput)
+    })
+    if (filterBooksData.length === 0) {
+      this.setState({
+        filterBooksData: ['查無資料']
+      })
+    } else {
+      this.setState({
+        filterBooksData: filterBooksData
+      })
+    };
+  };
+
   render() {
-    const { booksData, filterBooksData } = this.state;
+    const { booksData, filterBooksData, searchInput } = this.state;
     return (
       <div>
         <div className="header">
           <div className="header-text">天瓏書局 書籍查詢</div>
+        </div>
+        <div className="search-container">
+          <input className="search-bar" placeholder="請輸入書名" onChange={() => this.updateInput()}></input>
+          <button onClick={() => this.filterData()}>搜尋</button>
         </div>
         <BooksDisplay BooksData={this.state.filterBooksData} />
       </div>
