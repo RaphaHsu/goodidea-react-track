9027226725160c1b8fa4a21dffb5f931e4e3b6a8
mark9462 <jack9462@gmail.com>
2019-04-14 21:57:37 +0800

    finish search.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,47 @@
 import React, { Component } from "react";
-import { hot } from 'react-hot-loader/root';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import { hot } from "react-hot-loader/root";
 
-
-import Search from './search';
-import Products from './products';
-
-library.add(fab);
+import Search from "./search";
+import Products from "./products";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      booksData: [],
+      searchItems: [],
     };
   }
 
+  getBooksList = () => {
+    fetch("https://bookshelf.goodideas-studio.com/api")
+      .then(response => response.json())
+      .then((data) => {
+        const booksList = data.list;
+        this.setState({
+          booksData: booksList,
+          searchItems: booksList
+        });
+      })
+      .catch(error => console.log("Opps", error));
+  };
+
+  filterbooks = (searchItems) => {
+    this.setState({
+      searchItems
+    });
+  }
+
+  componentDidMount() {
+    this.getBooksList();
+  }
+
   render() {
+    const {booksData, searchItems} = this.state;
     return (
       <div>
-        <Search />
-        <Products />
+        <Search books={booksData} onChangeSearch={this.filterbooks} />
+        <Products searchItems={searchItems} />
       </div>
     );
   }
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const searchComponent = () => (
-  <div />
-);
+class searchComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: ""
+    };
+  }
+
+  filterData(e) {
+    const { books, onChangeSearch } = this.props;
+    let { searchInput } = this.state;
+    searchInput = e.target.value;
+    const filterResult = books.filter(
+      book => book.name.indexOf(searchInput) !== -1
+        || book.ISBN.indexOf(searchInput) !== -1
+    );
+    onChangeSearch(filterResult);
+  }
+
+  render() {
+    return (
+      <div>
+        <FontAwesomeIcon icon={faSearch} />
+        <input
+          type="text"
+          name="Text"
+          onChange={this.filterData}
+          placeholder="書籍名稱 or ISBN"
+        />
+      </div>
+    );
+  }
+
+  static propTypes = {
+    onChangeSearch: PropTypes.func.isRequired,
+    books: PropTypes.array.isRequired
+  };
+}
 export default searchComponent;
