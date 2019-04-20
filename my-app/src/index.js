b25cd9b2dd98add4c15091c6078425c623654ebb
chris <dwatow@gmail.com>
2019-04-20 12:24:03 +0800

    delete usless files

--- my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import HelloMessage from './HelloMessage'
-
-ReactDOM.render(
-  <HelloMessage name="Chris" />,
-  document.getElementById('root')
-);
