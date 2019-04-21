dea79e9629a252256e081df541b416f9b6605c68
chris <dwatow@gmail.com>
2019-04-21 08:17:54 +0800

    fix Warning: Invalid DOM property , class -> className in JSX

--- react-init/src/components/navigation.js
+++ react-init/src/components/navigation.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 import '@/assets/css/navigation.css'
 
-class News extends React.Component {
+class Navigation extends React.Component {
   render() {
     return (<div id="navigation_background">
-      <div id="navigation" class="clear_fix">
-        <ul class="menu horizontal_list_initial">
+      <div id="navigation" className="clear_fix">
+        <ul className="menu horizontal_list_initial">
           <li id="logo">PRETTY GAL</li>
-          <li class="menu_selected">Home</li>
+          <li className="menu_selected">Home</li>
           <li>Shop</li>
           <li>Sale</li>
           <li>Customer Care</li>
           <li>Stockists</li>
         </ul>
-        <img class="view_shopping_cart" src={require('@/assets/img/bag.png')} alt="view_shopping_cart"/>
+        <img className="view_shopping_cart" src={require('@/assets/img/bag.png')} alt="view_shopping_cart"/>
       </div>
     </div>)
   }
 }
 
-export default News
+export default Navigation
--- react-init/src/components/sale.js
+++ react-init/src/components/sale.js
@@ -5,13 +5,13 @@ import '@/assets/css/sale.css'
 class Sale extends React.Component {
   render() {
     return (<div id="sale_actie">
-      <div class="text_center">
-        <div class="sale_year_round">
+      <div className="text_center">
+        <div className="sale_year_round">
           <h1>YEAR ROUND</h1>
-          <div class="hr_line"></div>
+          <div className="hr_line"></div>
           <h2>Must Have Items</h2>
         </div>
-        <ul id="sale_img_list" class="horizontal_list_initial">
+        <ul id="sale_img_list" className="horizontal_list_initial">
           <li><img src={require('@/assets/img/left.png')}/></li>
           <li><img src={require('@/assets/img/middle.png')}/></li>
           <li><img src={require('@/assets/img/right.png')}/></li>
