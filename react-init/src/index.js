aac69f79964fb12ac2c27cc32716f36b4cfc4e53
chris <dwatow@gmail.com>
2019-04-21 16:22:09 +0800

    delete usless codes

--- react-init/src/index.js
+++ react-init/src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import HelloMessage from './HelloMessage'
 
 ReactDOM.render(
-  <HelloMessage name="Chris" />,
   document.getElementById('root')
 );
